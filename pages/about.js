d11f09f8b652938434271b965156f12b4bc08815
KelvinMitaki <mitakikelvin1@gmail.com>
2020-08-19 18:26:41 +0300

    populated about page with github data

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import Layout from "../components/Layout";
+import Axios from "axios";
 
-const about = () => {
-  return (
-    <Layout title="About Page">
-      <p>This is the about page</p>
-      <img src="/1.png" alt="js" />
-    </Layout>
-  );
-};
-
+export class about extends Component {
+  static async getInitialProps() {
+    const res = await Axios.get("https://api.github.com/users/KelvinMitaki");
+    return { user: res.data };
+  }
+  render() {
+    console.log(this.props.user);
+    const { bio, avatar_url, name } = this.props.user;
+    return (
+      <Layout title="About Page">
+        <h4>{name}</h4>
+        <p>{bio} </p>
+        <img src={avatar_url} alt={name} />
+      </Layout>
+    );
+  }
+}
 export default about;
