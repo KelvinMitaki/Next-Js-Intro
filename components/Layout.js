984c57b9d06c329acb6d3d4b76c49da340b205f8
KelvinMitaki <mitakikelvin1@gmail.com>
2020-08-19 17:20:20 +0300

    added styling to the app

--- components/Layout.js
+++ components/Layout.js
@@ -2,12 +2,62 @@ import React from "react";
 import Link from "next/link";
 const Layout = ({ children, title }) => {
   return (
-    <div>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <header>{title}</header>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column"
+      }}
+    >
+      <header
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-around",
+          padding: "1em",
+          fontSize: "1.2rem",
+          background: "indigo"
+        }}
+      >
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+        <Link href="/hireme">
+          <a>Hire Me</a>
+        </Link>
+      </header>
+      <h1>{title}</h1>
       {children}
       <footer>&copy; {new Date().getFullYear()}</footer>
+      <style jsx>
+        {`
+          header a {
+            color: darkgrey;
+            text-decoration: none;
+          }
+          header a:hover {
+            font-weight: bold;
+            color: lightgrey;
+          }
+          footer {
+            padding: 1em;
+          }
+        `}
+      </style>
+
+      <style global jsx>
+        {`
+          body {
+            margin: 0;
+            font-size: 110%;
+            background: #fofofo;
+          }
+        `}
+      </style>
     </div>
   );
 };
---
+++ pages/hireme.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const hireme = () => {
+  return (
+    <Layout title="hire me">
+      <p>You can hire me at mitakikelvin1@gmail.com</p>
+    </Layout>
+  );
+};
+
+export default hireme;
