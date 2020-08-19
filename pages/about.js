3772d62ccdd61aa1988578b3abe4f5608449f7ec
KelvinMitaki <mitakikelvin1@gmail.com>
2020-08-19 16:42:24 +0300

    added about page

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <div>
+      <h2>About page</h2>
+      <p>This is the about page</p>
+    </div>
+  );
+};
+
+export default about;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const index = () => {
-  return <div>hello</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>this is the home page</p>
+    </div>
+  );
 };
 
 export default index;
